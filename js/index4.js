ddf0ebb033196d975a581d085fa8647abfdae81d
Constanza Repetto <repettocj@gmail.com>
2022-08-23 20:53:56 -0300

    Entregable 23/08

--- js/index4.js
+++ js/index4.js
@@ -13,11 +13,54 @@
 
 //Funcion con retorno
 
-let primerNum = 0;
-let segundoNum = 0;
+// let primerNum = 0;
+// let segundoNum = 0;
 
-function calcular(primerNum, segundoNum) {//recibe parametros
-    return primerNum * segundoNum;
+// function calcular(primerNum, segundoNum) { //recibe parametros
+//     return primerNum * segundoNum;
+// }
+
+// let resultado = calcular();
+
+//
+
+// function calculos() {
+//     let primerNro = parseFloat(prompt('Ingresa el 1er nro:'))
+//     let segundoNro = parseFloat(prompt('Ingresa el 1er nro:'))
+//     let operacion = prompt('Ingresa la operacion (+ - * /):')
+//     console.log('Resultado', realizarCulculo(primerNro, segundoNro, operacion))
+// }
+
+// function realizarCulculo(num1, num2, ope) {
+//     switch (ope) {
+//         case '+':
+//             return num1 + num2
+//         case '-':
+//             return num1 - num2
+//         case '*':
+//             return num1 * num2
+//         case '/':
+//             return num1 / num2
+//     }
+// }
+
+//Funciones ANONIMAS
+let iva = 1.21
+
+const precioConIVA = function(precio, iva){
+    return precio * iva
 }
 
-let resultado = calcular();
+/* Llamado
+precioConIVA(120, iva)
+*/
+
+//Funcion FLECHA
+
+function cuandoNaciste(){
+    console.log(2022 - 45)
+}
+
+const cuandoNacio = () => {
+    console.log(2022 - 45)
+}
